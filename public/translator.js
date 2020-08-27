7299acbbe230c5d6adde43577d7e0c8066ac37f9
Glitch (enamarat-american-british-english-translator) <none>
2020-08-27 14:23:38 +0000

    🗃🐣 Updated with Glitch

--- public/translator.js
+++ public/translator.js
@@ -3,6 +3,159 @@ import { britishOnly } from './british-only.js';
 import { americanToBritishSpelling } from './american-to-british-spelling.js';
 import { americanToBritishTitles } from './american-to-british-titles.js';
 
+const textArea = document.getElementById('text-input');
+const errorDiv = document.getElementById('error-msg');
+const translatedDiv = document.getElementById('translated-sentence');
+const selectedOption = document.getElementById('locale-select');
+
+/*** functions ***/
+///////////////////
+const translateText = (input) => {
+  /**asses input's length**/
+  errorDiv.textContent = "";
+  if (input.length === 0) {
+    const message = "Error: No text to translate.";
+    errorDiv.textContent = message;
+    return message;
+  }
+  
+  let output = "";
+  let inputToModify = input;
+  let wordToSubstituteDetected = false;
+
+   ////////////////////////////////////////////////////
+   //translate from American to British
+  if (selectedOption.value === "american-to-british") {
+    // words
+    for (let property in americanToBritishSpelling) {
+      let americanWord = property;
+      let regex = new RegExp("\\b" + americanWord + "\\b","gi");
+      if (regex.test(inputToModify) === true) {
+        wordToSubstituteDetected = true;
+        output = inputToModify.replace(regex, `<span class='highlight'>${americanToBritishSpelling[property]}</span>`);
+        inputToModify = output;
+      }
+    }
+    
+     for (let property in americanOnly) {
+      let americanWord = property;
+      let regex = new RegExp("\\b" + americanWord + "\\b","gi");
+      if (regex.test(inputToModify) === true) {
+        wordToSubstituteDetected = true;
+        output = inputToModify.replace(regex, `<span class='highlight'>${americanOnly[property]}</span>`);
+        inputToModify = output;
+      }
+    }
+    
+    // titles
+    for (let property in americanToBritishTitles) {
+      let americanWord = property;
+      let regex = new RegExp("\\b" + americanWord + "." + "\\b","gi");
+        //let regex = new RegExp(americanWord,"gi");
+      if (regex.test(inputToModify) === true) {
+        wordToSubstituteDetected = true;
+        output = inputToModify.replace(regex, `<span class='highlight'>${americanToBritishTitles[property][0].toUpperCase() + americanToBritishTitles[property].substring(1)} </span>`);
+        inputToModify = output;
+      }
+    }
+    
+    // time
+    const regexAmericanTime = /\d{2}:\d{2}/g;
+    if (regexAmericanTime.test(inputToModify) === true) {
+      const americanTimeStamps = inputToModify.match(regexAmericanTime);
+      const britishTimeStamps = [];
+      
+      americanTimeStamps.forEach((stamp) => {
+        britishTimeStamps.push(stamp.split(":").join("."));
+      });
+      
+      for (let i = 0; i < americanTimeStamps.length; i++) {
+        let regex = new RegExp(americanTimeStamps[i],"i");
+        output = inputToModify.replace(regex, `<span class='highlight'>${britishTimeStamps[i]}</span>`);
+        inputToModify = output;
+      }
+      wordToSubstituteDetected = true;
+    }
+  }
+  
+  ////////////////////////////////////////////////////
+  //translate from British to American
+  if (selectedOption.value === "british-to-american") {
+    // words
+    for (let property in americanToBritishSpelling) {
+      let britishWord = americanToBritishSpelling[property];
+      let regex = new RegExp("\\b" + britishWord + "\\b","gi");
+      if (regex.test(inputToModify) === true) {
+        wordToSubstituteDetected = true;
+        output = inputToModify.replace(regex, `<span class='highlight'>${property}</span>`);
+        inputToModify = output;
+      }
+    }
+    
+     for (let property in britishOnly) {
+      let britishWord = property;
+      let regex = new RegExp("\\b" + britishWord + "\\b","gi");
+      if (regex.test(inputToModify) === true) {
+        wordToSubstituteDetected = true;
+        output = inputToModify.replace(regex, `<span class='highlight'>${britishOnly[property]}</span>`);
+        inputToModify = output;
+      }
+    }
+    
+    // titles
+    for (let property in americanToBritishTitles) {
+      let britishWord = americanToBritishTitles[property];
+      let regex = new RegExp("\\b" + britishWord + "\\b","gi");
+      if (regex.test(inputToModify) === true) {
+        wordToSubstituteDetected = true;
+        output = inputToModify.replace(regex, `<span class='highlight'>${property}</span>`);
+        inputToModify = output;
+      }
+    }
+    
+    // time
+    const regexAmericanTime = /\d{2}.\d{2}/g;
+    if (regexAmericanTime.test(inputToModify) === true) {
+      const americanTimeStamps = inputToModify.match(regexAmericanTime);
+      const britishTimeStamps = [];
+      
+      americanTimeStamps.forEach((stamp) => {
+        britishTimeStamps.push(stamp.split(".").join(":"));
+      });
+      
+      for (let i = 0; i < americanTimeStamps.length; i++) {
+        let regex = new RegExp(americanTimeStamps[i],"i");
+        output = inputToModify.replace(regex, `<span class='highlight'>${britishTimeStamps[i]}</span>`);
+        inputToModify = output;
+      }
+      wordToSubstituteDetected = true;
+    }
+  }
+  
+  ////////////////////////////////////////////////////
+   if (wordToSubstituteDetected === true) {
+      translatedDiv.innerHTML = output;
+    } else {
+      translatedDiv.textContent = "Everything looks good to me!";
+    }
+    return translatedDiv.textContent;
+}
+
+///////////////////
+const clearTextArea = () => {
+  textArea.value = "";
+  errorDiv.textContent = "";
+  translatedDiv.textContent = "";
+}
+
+document.getElementById('translate-btn').addEventListener('click', () => {
+  translateText(textArea.value);
+});
+
+document.getElementById('clear-btn').addEventListener('click', () => {
+  clearTextArea();
+});
+
 /* 
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
@@ -10,6 +163,6 @@ import { americanToBritishTitles } from './american-to-british-titles.js';
 */
 try {
   module.exports = {
-
+    translateText
   }
 } catch (e) {}
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -12,7 +12,8 @@ const assert = chai.assert;
 const { JSDOM } = require('jsdom');
 let Translator;
 
-suite('Unit Tests', () => {
+suite('Unit Tests', function() {
+   this.timeout(0);
   suiteSetup(() => {
     // Mock the DOM for testing and load Translator
     return JSDOM.fromFile('./views/index.html')
@@ -24,78 +25,79 @@ suite('Unit Tests', () => {
       });
   });
 
-  suite('Function ____()', () => {
+  suite('Function translateText()', () => {
 
     suite('American to British English', () => {
 
       test('Mangoes are my favorite fruit. --> Mangoes are my favourite fruit.', done => {
         const input = 'Mangoes are my favorite fruit.';
         const output = 'Mangoes are my favourite fruit.';
-
-        // done();
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
 
       test('I ate yogurt for breakfast. --> I ate yoghurt for breakfast.', done => {
         const input = 'I ate yogurt for breakfast.';
         const output = 'I ate yoghurt for breakfast.';
-
-        // done();
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
 
       test("We had a party at my friend's condo. --> We had a party at my friend's flat.", done => {
         const input = "We had a party at my friend's condo.";
         const output = "We had a party at my friend's flat.";
-
-        // done();
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
 
       test('Can you toss this in the trashcan for me? --> Can you toss this in the bin for me?', done => {
         const input = 'Can you toss this in the trashcan for me?';
         const output = 'Can you toss this in the bin for me?';
-
-        // done();
+        //
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
 
       test('The parking lot was full. --> The car park was full.', done => {
         const input = 'The parking lot was full.';
         const output = 'The car park was full.';
-
-        // done();
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
 
       test('Like a high tech Rube Goldberg machine. --> Like a high tech Heath Robinson device.', done => {
         const input = 'Like a high tech Rube Goldberg machine.';
         const output = 'Like a high tech Heath Robinson device.';
-
-        // done();
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
       
       test('To play hooky means to skip class or work. --> To bunk off means to skip class or work.', done => {
         const input = 'To play hooky means to skip class or work.';
         const output = 'To bunk off means to skip class or work.';
-
-        // done();
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
 
       test('No Mr. Bond, I expect you to die. --> No Mr Bond, I expect you to die. ', done => {
         const input = 'No Mr. Bond, I expect you to die.';
         const output = 'No Mr Bond, I expect you to die.';
-
-        // done();
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
 
       test('Dr. Grosh will see you now. --> Dr Grosh will see you now. ', done => {
         const input = 'Dr. Grosh will see you now.';
         const output = 'Dr Grosh will see you now.';
-
-        // done();
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
 
       test('Lunch is at 12:15 today. --> Lunch is at 12.15 today.', done => {
         const input = 'Lunch is at 12:15 today.';
         const output = 'Lunch is at 12.15 today.';
-        
-        // done();
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
 
     });
@@ -105,8 +107,8 @@ suite('Unit Tests', () => {
       test('We watched the footie match for a while. --> We watched the soccer match for a while.', done => {
         const input = 'We watched the footie match for a while.';
         const output = 'We watched the soccer match for a while.';
-
-        // done();
+        assert.equal(Translator.translateText(input), output);
+        done();
       });
 
       test('Paracetamol takes up to an hour to work. --> Tylenol takes up to an hour to work.', done => {
